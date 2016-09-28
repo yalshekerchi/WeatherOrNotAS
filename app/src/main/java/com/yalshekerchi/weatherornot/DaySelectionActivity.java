8735262c398b9ceca2b65b0e6e7b540593dd2375
yalshekerchi <yalshekerchi@outlook.com>
2016-09-27 23:09:57 -0400

    Added Recycler View and Card View for second activity (Needs work)

---
+++ app/src/main/java/com/yalshekerchi/weatherornot/DaySelectionActivity.java
@@ -0,0 +1,85 @@
+package com.yalshekerchi.weatherornot;
+
+//Support Libraries
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+//Utility Libraries
+import java.util.List;
+import java.util.Locale;
+import android.view.View;
+import android.Manifest;
+import android.util.Log;
+
+//Widget Libraries
+import android.widget.Toast;
+import android.widget.TextView;
+import android.widget.NumberPicker;
+import android.widget.Button;
+
+//Card View Libraries
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import java.util.ArrayList;
+
+public class DaySelectionActivity extends AppCompatActivity {
+    //Get Variables passed from MainActivity
+    Bundle extras = getIntent().getExtras();
+    String valLatitude = extras.getString("valLatitude");
+    String valLongitude = extras.getString("valLongitude");
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private static String LOG_TAG = "DaySelectionActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_day_selection);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mAdapter = new MyRecyclerViewAdapter(getDataSet());
+        mRecyclerView.setAdapter(mAdapter);
+
+        // Code to Add an item with default animation
+        //((MyRecyclerViewAdapter) mAdapter).addItem(obj, index);
+
+        // Code to remove an item with default animation
+        //((MyRecyclerViewAdapter) mAdapter).deleteItem(index);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ((MyRecyclerViewAdapter) mAdapter).setOnItemClickListener(new MyRecyclerViewAdapter
+                .MyClickListener() {
+            @Override
+            public void onItemClick(int position, View v) {
+                Log.i(LOG_TAG, " Clicked on Item " + position);
+            }
+        });
+    }
+
+    private ArrayList<DataObject> getDataSet() {
+        ArrayList results = new ArrayList<DataObject>();
+        for (int index = 0; index < 20; index++) {
+            DataObject obj = new DataObject("Some Primary Text " + index,
+                    "Secondary " + index);
+            results.add(index, obj);
+        }
+        return results;
+    }
+}
